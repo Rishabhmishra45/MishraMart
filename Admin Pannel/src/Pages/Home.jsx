738ddf8cc7670e4289689f05ef880f20fff4194b
Rishabh <rishabhmishra872660@gmail.com>
2025-09-21 23:14:38 +0530

    admin backend

--- Admin Pannel/src/Pages/Home.jsx
+++ Admin Pannel/src/Pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Nav from "../components/Nav"
 
 const Home = () => {
   return (
     <div className='w-[100vw] h-[100vh] bg-gradient-to-l from-[#141414] to-[#0c2025] text-[white] relative'>
-
+      <Nav />
     </div>
   )
 }
--- Admin Pannel/src/components/Nav.jsx
+++ Admin Pannel/src/components/Nav.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import logo from '../assets/logo.png';
+import { authDataContext } from '../context/AuthContext';
+import { adminDataContext } from '../context/AdminContext';
 
 const Nav = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const navigate = useNavigate();
+  const { serverUrl } = useContext(authDataContext);
+  const { getAdmin } = useContext(adminDataContext);
 
-export default Nav
+  const logOut = async () => {
+    try {
+      const result = await axios.get(serverUrl + '/api/auth/logout', {
+        withCredentials: true,
+      });
+      console.log(result.data);
+      getAdmin();
+      navigate('/login');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="w-full h-[60px] bg-[#dcdbdbf8] fixed top-0 left-0 z-10 flex       items-center justify-between px-4 md:px-8 shadow-md shadow-black select-none">
+      {/* Logo + text */}
+      <div
+        className="flex items-center gap-2 cursor-pointer"
+        onClick={() => navigate('/')}
+      >
+        <div className="flex-shrink-0 flex items-center h-[150px]">
+          <img
+            src={logo}
+            alt="Logo"
+            draggable={false}
+            className="max-h-[180px] h-full w-auto object-contain"
+          />
+        </div>
+      </div>
+
+      {/* Logout button */}
+      <button
+        onClick={logOut}
+        className="bg-black text-white px-4 py-2 rounded-2xl hover:bg-gray-800 transition-colors text-sm sm:text-base
+        "
+      >
+        LogOut
+      </button>
+    </div>
+  );
+};
+
+export default Nav;
