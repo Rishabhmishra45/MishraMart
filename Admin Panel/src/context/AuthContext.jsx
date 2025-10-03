b55131e48e63dd02f6abdb3a41eb76da93c076d7
Rishabh <rishabhmishra872660@gmail.com>
2025-10-03 15:49:22 +0530

    Deployment

--- Admin Panel/src/context/AuthContext.jsx
+++ Admin Panel/src/context/AuthContext.jsx
@@ -1,18 +1,19 @@
 import React, { createContext } from 'react'
 
 export const authDataContext = createContext()
+
 const AuthContext = ({ children }) => {
-    let serverUrl = "http://localhost:8000"
+
+    let serverUrl = import.meta.env.VITE_SERVER_URL
 
     let value = {
         serverUrl
     }
+
     return (
-        <div>
-            <authDataContext.Provider value={value}>
-                {children}
-            </authDataContext.Provider>
-        </div>
+        <authDataContext.Provider value={value}>
+            {children}
+        </authDataContext.Provider>
     )
 }
 
