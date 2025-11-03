361c7d6aea2f86231873dae9a2043e6571d9e57b
Rishabh <rishabhmishra872660@gmail.com>
2025-11-04 00:01:58 +0530

    Add for guest user

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
   plugins: [
     react(),
@@ -11,6 +10,9 @@ export default defineConfig({
   server: {
     host: true,   
     port: 5173  
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false
   }
 })
-
