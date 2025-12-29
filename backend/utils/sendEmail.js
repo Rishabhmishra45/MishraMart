fd427095727cf8f0101dcf0eeb251182b3f7507c
Rishabh <rishabhmishra872660@gmail.com>
2025-12-29 15:51:01 +0530

    forget password

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,14 +4,17 @@ const sendEmail = async ({ email, subject, html }) => {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: false, // IMPORTANT for production
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
-    connectionTimeout: 10000, // 10 sec
-    greetingTimeout: 10000,
-    socketTimeout: 10000,
+    tls: {
+      rejectUnauthorized: false,
+    },
+    connectionTimeout: 20000, // ⬅️ 20 sec
+    greetingTimeout: 20000,
+    socketTimeout: 20000,
   });
 
   await transporter.sendMail({
